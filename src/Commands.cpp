f66f1c511c93bed920c3ece1be499ba69c028de3
Mimovnik-Manjaro <jkwidzinski@gmail.com>
2022-05-13 12:19:40 +0200

    Print end line after possible moves count properly

--- src/Commands.cpp
+++ src/Commands.cpp
@@ -3,11 +3,13 @@
 #include <iostream>
 
 void genAllPosMov() {
+    // n- row length, m- rows count
+    // k- number of adjacent cells in continuous vertical, horizontal or
+    // diagonal line
     int n, m, k;
     int posMoves = 0;
-    char playerSign;
-    std::cin >> n >> m >> k >> playerSign;
-
+    char activePlayer;
+    std::cin >> n >> m >> k >> activePlayer;
 
     char* board = new char[n * m];
     for (int i = 0; i < n * m; i++) {
@@ -17,10 +19,10 @@ void genAllPosMov() {
         }
     }
 
-    std::cout << posMoves;
+    std::cout << posMoves << std::endl;
     for (int i = 0; i < n * m; i++) {
         if (board[i] == '0') {
-            board[i] = playerSign;
+            board[i] = activePlayer;
             for (int y = 0; y < m; y++) {
                 for (int x = 0; x < n; x++) {
                     std::cout << board[n * y + x] << ' ';
@@ -28,8 +30,8 @@ void genAllPosMov() {
                 std::cout << std::endl;
             }
             board[i] = '0';
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
     }
 
     delete[] board;
