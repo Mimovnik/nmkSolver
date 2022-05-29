ca75d97e6a2aaa41794bf1bc6b0c07b3bef549bf
Mimovnik-Manjaro <jkwidzinski@gmail.com>
2022-05-29 17:03:43 +0200

    Define magic variables, create functions headers

--- src/Commands.cpp
+++ src/Commands.cpp
@@ -6,84 +6,101 @@
 #define EMPTY_CELL '0'
 #define PLAYER1 '1'
 #define PLAYER2 '2'
+#define INFINITY 100
 
 char opponent(char activePlayer) {
-    if (activePlayer == '1') {
-        return '2';
+    if (activePlayer == PLAYER1) {
+        return PLAYER2;
     }
-    return '1';
+    return PLAYER1;
 }
 
-char getCell(char* board, int rowWidth, int x, int y) {
-    return board[y * rowWidth + x];
+char getCell(char* board, int width, int x, int y) {
+    return board[y * width + x];
 }
 
-bool won(char player, char* board, int width, int height,
-         int consecutiveToWin) {
-    // for all cells
+void printBoard(int width, int height, char* board) {
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            // check possible continuous lines in all directions
-            int d = 4;
-            while (d--) {
-                int continuousSigns = 0;
-                for (int l = 0; l < consecutiveToWin; l++) {
-                    switch (d) {
-                        // right
-                        case 3:
-                            if (x + l >= width) {
-                                break;
-                            }
-                            if (getCell(board, width, x + l, y) == player) {
-                                continuousSigns++;
-                            }
-                            break;
-                        // right down
-                        case 2:
-                            if (x + l >= width || y + l >= height) {
-                                break;
-                            }
-                            if (getCell(board, width, x + l, y + l) == player) {
-                                continuousSigns++;
-                            }
-                            break;
-                        // down
-                        case 1:
-                            if (y + l >= height) {
-                                break;
-                            }
-                            if (getCell(board, width, x, y + l) == player) {
-                                continuousSigns++;
-                            }
-                            break;
-                        // down left
-                        case 0:
-                            if (x - l < 0 || y + l >= height) {
-                                break;
-                            }
-                            if (getCell(board, width, x - l, y + l) == player) {
-                                continuousSigns++;
-                            }
-                            break;
-                    }
-                    if (continuousSigns == consecutiveToWin) {
-                        return true;
-                    }
-                }
+            std::cout << getCell(board, width, x, y);
+            if (x != width - 1) {
+                std::cout << " ";
             }
         }
+        std::cout << std::endl;
     }
-    return false;
 }
 
-char getConsecutive(char* arr, int size, int amount, int stepsize) {
-    if (size < amount) {
-        return '0';
+int countMovesAndCheckIfWinner(char* board, int width, int height,
+                               int consecutiveToWin, char activePlayer,
+                               bool cut, bool* isWinner) {
+    int posMoves = 0;
+    for (int i = 0; i < width * height; i++) {
+        if (board[i] == EMPTY_CELL) {
+            posMoves++;
+            board[i] = activePlayer;
+            if (cut && getWinner(board, width, height, consecutiveToWin) ==
+                           activePlayer) {
+                *isWinner = true;
+                return 1;
+            }
+            board[i] = EMPTY_CELL;
+        }
     }
-    char last = '0';
+    return posMoves;
+}
+
+void printAllPosMov(char* board, int width, int height, char activePlayer) {
+    for (int i = 0; i < width * height; i++) {
+        if (board[i] == EMPTY_CELL) {
+            board[i] = activePlayer;
+            printBoard(width, height, board);
+            board[i] = EMPTY_CELL;
+        }
+    }
+}
+
+void genAllPosMov(bool cut) {
+    // consecutiveToWin- number of adjacent cells in continuous vertical,
+    // horizontal or diagonal line required to win
+    int width, height, consecutiveToWin;
+    char activePlayer;
+    std::cin >> height >> width >> consecutiveToWin >> activePlayer;
+
+    char* board = new char[width * height];
+    for (int i = 0; i < width * height; i++) {
+        std::cin >> board[i];
+    }
+
+    if (getWinner(board, width, height, consecutiveToWin) ==
+        opponent(activePlayer)) {
+        std::cout << 0 << std::endl;
+        delete[] board;
+        return;
+    }
+    bool isWinner = false;
+    std::cout << countMovesAndCheckIfWinner(board, width, height,
+                                            consecutiveToWin, activePlayer, cut,
+                                            &isWinner)
+              << std::endl;
+
+    if (isWinner) {
+        printBoard(width, height, board);
+    } else {
+        printAllPosMov(board, width, height, activePlayer);
+    }
+
+    delete[] board;
+}
+
+char getConsecutive(char* arr, int stepCount, int amount, int stepsize) {
+    if (stepCount < amount) {
+        return NULL;
+    }
+    char last = NULL;
     int count = 0;
-    for (int i = 0, x = 0; i < size; i++, x += stepsize) {
-        if (arr[x] != last || arr[x] == '0') {
+    for (int i = 0, x = 0; i < stepCount; i++, x += stepsize) {
+        if (arr[x] != last || arr[x] == EMPTY_CELL) {
             last = arr[x];
             count = 0;
         }
@@ -92,7 +109,7 @@ char getConsecutive(char* arr, int size, int amount, int stepsize) {
             return arr[x];
         }
     }
-    return '0';
+    return NULL;
 }
 
 char getWinner(char* board, int width, int height, int consecutiveToWin) {
@@ -103,7 +120,7 @@ char getWinner(char* board, int width, int height, int consecutiveToWin) {
     for (int y = 0; y < height; y++) {
         char win =
             getConsecutive(board + y * width, width, consecutiveToWin, 1);
-        if (win != '0') {
+        if (win != NULL) {
             return win;
         }
     }
@@ -113,7 +130,7 @@ char getWinner(char* board, int width, int height, int consecutiveToWin) {
     // |00
     for (int x = 0; x < width; x++) {
         char win = getConsecutive(board + x, height, consecutiveToWin, width);
-        if (win != '0') {
+        if (win != NULL) {
             return win;
         }
     }
@@ -127,7 +144,7 @@ char getWinner(char* board, int width, int height, int consecutiveToWin) {
         char win = getConsecutive(
             width > height ? board + d : board + d * width,
             std::min(width, height), consecutiveToWin, width + 1);
-        if (win != '0') {
+        if (win != NULL) {
             return win;
         }
     }
@@ -149,7 +166,7 @@ char getWinner(char* board, int width, int height, int consecutiveToWin) {
         char win = getConsecutive(board + width * (s + leftBotAdd),
                                   std::min(width, height) - s, consecutiveToWin,
                                   width + 1);
-        if (win != '0') {
+        if (win != NULL) {
             return win;
         }
         // right top diagonals
@@ -159,7 +176,7 @@ char getWinner(char* board, int width, int height, int consecutiveToWin) {
         win =
             getConsecutive(board + s + rightTopAdd, std::min(width, height) - s,
                            consecutiveToWin, width + 1);
-        if (win != '0') {
+        if (win != NULL) {
             return win;
         }
     }
@@ -173,7 +190,7 @@ char getWinner(char* board, int width, int height, int consecutiveToWin) {
             width > height ? board + width - 1 - d
                            : board + width - 1 + d * width,
             std::min(width, height), consecutiveToWin, width - 1);
-        if (win != '0') {
+        if (win != NULL) {
             return win;
         }
     }
@@ -195,7 +212,7 @@ char getWinner(char* board, int width, int height, int consecutiveToWin) {
         char win = getConsecutive(board + width - 1 - (s + leftTopAdd),
                                   std::min(width, height) - s, consecutiveToWin,
                                   width - 1);
-        if (win != '0') {
+        if (win != NULL) {
             return win;
         }
         // right bottom diagonals
@@ -205,20 +222,20 @@ char getWinner(char* board, int width, int height, int consecutiveToWin) {
         win = getConsecutive(board + width - 1 + width * (s + rightBotAdd),
                              std::min(width, height) - s, consecutiveToWin,
                              width - 1);
-        if (win != '0') {
+        if (win != NULL) {
             return win;
         }
     }
-    return '0';
+    return NULL;
 }
 
 int* getScore(char* board, int width, int height, int consecutiveToWin) {
     char winner = getWinner(board, width, height, consecutiveToWin);
 
     switch (winner) {
-        case '0':
+        case NULL:
             for (int i = 0; i < width * height; i++) {
-                if (board[i] == '0') {
+                if (board[i] == EMPTY_CELL) {
                     return nullptr;
                 }
             }
@@ -233,79 +250,6 @@ int* getScore(char* board, int width, int height, int consecutiveToWin) {
     return nullptr;
 }
 
-void printBoard(int width, int height, char* board) {
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            std::cout << getCell(board, width, x, y);
-            if (x != width - 1) {
-                std::cout << " ";
-            }
-        }
-        std::cout << std::endl;
-    }
-}
-
-int countMovesAndCheckIfWinner(char* board, int width, int height,
-                               int consecutiveToWin, char activePlayer,
-                               bool cut, bool* isWinner) {
-    int posMoves = 0;
-    for (int i = 0; i < width * height; i++) {
-        if (board[i] == '0') {
-            posMoves++;
-            board[i] = activePlayer;
-            if (cut &&
-                won(activePlayer, board, width, height, consecutiveToWin)) {
-                *isWinner = true;
-                return 1;
-            }
-            board[i] = '0';
-        }
-    }
-    return posMoves;
-}
-
-void printAllPosMov(char* board, int width, int height, char activePlayer) {
-    for (int i = 0; i < width * height; i++) {
-        if (board[i] == '0') {
-            board[i] = activePlayer;
-            printBoard(width, height, board);
-            board[i] = '0';
-        }
-    }
-}
-
-void genAllPosMov(bool cut) {
-    // consecutiveToWin- number of adjacent cells in continuous vertical,
-    // horizontal or diagonal line required to win
-    int width, height, consecutiveToWin;
-    char activePlayer;
-    std::cin >> height >> width >> consecutiveToWin >> activePlayer;
-
-    char* board = new char[width * height];
-    for (int i = 0; i < width * height; i++) {
-        std::cin >> board[i];
-    }
-
-    if (won(opponent(activePlayer), board, width, height, consecutiveToWin)) {
-        std::cout << 0 << std::endl;
-        delete[] board;
-        return;
-    }
-    bool isWinner = false;
-    std::cout << countMovesAndCheckIfWinner(board, width, height,
-                                            consecutiveToWin, activePlayer, cut,
-                                            &isWinner)
-              << std::endl;
-
-    if (isWinner) {
-        printBoard(width, height, board);
-    } else {
-        printAllPosMov(board, width, height, activePlayer);
-    }
-
-    delete[] board;
-}
-
 int minimax(char* board, int width, int height, int consecutiveToWin, int depth,
             int alpha, int beta, bool isMaximizing) {
     int* result = getScore(board, width, height, consecutiveToWin);
@@ -315,7 +259,7 @@ int minimax(char* board, int width, int height, int consecutiveToWin, int depth,
         return score;
     }
     if (isMaximizing) {
-        int bestScore = -100;
+        int bestScore = -INFINITY;
         for (int i = 0; i < width * height; i++) {
             if (board[i] == EMPTY_CELL) {
                 board[i] = PLAYER2;
@@ -331,7 +275,7 @@ int minimax(char* board, int width, int height, int consecutiveToWin, int depth,
         }
         return bestScore;
     } else {
-        int bestScore = 100;
+        int bestScore = INFINITY;
         for (int i = 0; i < width * height; i++) {
             if (board[i] == EMPTY_CELL) {
                 board[i] = PLAYER1;
@@ -364,7 +308,7 @@ void solveGame() {
         std::cin >> board[i];
     }
 
-    int score = minimax(board, width, height, consecutiveToWin, 0, -100, 100,
+    int score = minimax(board, width, height, consecutiveToWin, 0, -INFINITY, INFINITY,
                         isMaximizing);
     switch (score) {
         case 0:
@@ -379,4 +323,6 @@ void solveGame() {
         default:
             throw "There is no such score! minimax() has a bug";
     }
+
+    delete[] board;
 }
--- src/Commands.h
+++ src/Commands.h
@@ -1,9 +1,29 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
-
 void genAllPosMov(bool cut);
 
 void solveGame();
+
+int minimax(char* board, int width, int height, int consecutiveToWin, int depth,
+            int alpha, int beta, bool isMaximizing);
+
+int* getScore(char* board, int width, int height, int consecutiveToWin);
+
+char getWinner(char* board, int width, int height, int consecutiveToWin);
+
+char opponent(char activePlayer);
+
+char getCell(char* board, int width, int x, int y);
+
+void printBoard(int width, int height, char* board);
+
+int countMovesAndCheckIfWinner(char* board, int width, int height,
+                               int consecutiveToWin, char activePlayer,
+                               bool cut, bool* isWinner);
+
+void printAllPosMov(char* board, int width, int height, char activePlayer);
+
+char getConsecutive(char* arr, int stepCount, int amount, int stepSize);
 
 #endif
